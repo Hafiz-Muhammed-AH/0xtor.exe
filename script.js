b59b47a8aeefc268f740ed45c89b95203100421c
0xtor.exe <99325825+Hafiz-Muhammed-AH@users.noreply.github.com>
2025-01-07 20:14:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+// Sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// Sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+// Page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// Add event listener to all navigation links
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default anchor behavior
+
+    // Get the target page from the link's text
+    const targetPage = this.innerText.toLowerCase();
+
+    // Toggle active class on the matching page and navigation link
+    for (let j = 0; j < pages.length; j++) {
+      if (pages[j].dataset.page === targetPage) {
+        pages[j].classList.add("active");
+        navigationLinks[j].classList.add("active");
+      } else {
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
+      }
+    }
+
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+  });
+}
+
+// Contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// Add event to all form input fields
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+    // Check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+}
